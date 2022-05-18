dc8e00ced80ac4ab4531c0d46dc34db7cb7f2a71
bernhardstandl <51175422+bernhardstandl@users.noreply.github.com>
2022-05-18 10:51:34 +0200

    Create coding_test.py

---
+++ coding_test.py
@@ -0,0 +1,16 @@
+print("Aufgabe 1")
+#Aufgabe 1
+for i in range(1,20):
+  print(i,end=",")
+
+print("\n\nAufgabe 2")
+#Aufgabe 2
+for i in range(1,20):
+  if((i%2)==0):
+    print(i,end=",")
+    
+print("\n\nAufgabe 3")
+#Aufgabe 3
+for i in range(1,20):
+  if(((i%2)==0) and (i%3)==0):
+    print(i, " ist durch 2 und 3 teilbar.")
