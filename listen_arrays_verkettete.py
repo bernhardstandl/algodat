792fdc5ca988cf6db8c728d5e6329e5a4d4163c0
Bernhard Standl <51175422+bernhardstandl@users.noreply.github.com>
2023-06-21 06:14:28 +0200

    Update listen_arrays_verkettete.py

--- listen_arrays_verkettete.py
+++ listen_arrays_verkettete.py
@@ -5,40 +5,17 @@ import random
 
 
 # Standard Liste in Python (Dynamisches Array)
-start_time = time.time()
-
-my_list = []
+dynamisches_array = []
 for _ in range(1000):
     my_list.append(random.randint(1, 100))
-
-end_time = time.time()
-list_time = end_time - start_time
 
 
 
 # Verkettte Liste mit dem Modul LLIST
-start_time = time.time()
-
-my_llist = sllist()
+verkettete_liste = sllist()
 for _ in range(1000):
     my_llist.append(random.randint(1, 100))
 
-end_time = time.time()
-llist_time = end_time - start_time
-
-
 
 # Array mit Numpy
-start_time = time.time()
-
-my_array = np.random.randint(1, 100, 1000)
-
-end_time = time.time()
-array_time = end_time - start_time
-
-
-
-# Printing the time taken for each method
-print("Time taken to create list (default):", list_time)
-print("Time taken to create list (llist module):", llist_time)
-print("Time taken to create array (NumPy):", array_time)
+array = np.random.randint(1, 100, 1000)
