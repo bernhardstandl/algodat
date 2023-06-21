9d1c72d88460a846ea07a185058f3a8c41f34c2d
Bernhard Standl <51175422+bernhardstandl@users.noreply.github.com>
2023-06-21 06:25:22 +0200

    Update listen_arrays_verkettete.py

--- listen_arrays_verkettete.py
+++ listen_arrays_verkettete.py
@@ -1,20 +1,18 @@
-import time
 from llist import sllist
 import numpy as np
-import random
 
 
 # Standard Liste in Python (Dynamisches Array)
 dynamisches_array = []
 for _ in range(1000):
-    my_list.append(random.randint(1, 100))
+    dynamisches_array.append(random.randint(1, 100))
 
 
 
 # Verkettte Liste mit dem Modul LLIST
 verkettete_liste = sllist()
 for _ in range(1000):
-    my_llist.append(random.randint(1, 100))
+    verkettete_liste.append(random.randint(1, 100))
 
 
 # Array mit Numpy
